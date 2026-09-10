53254cbc1bba94f2674a34979f7fd7f80ea6ed52
tocchi9755 <tocchi9755@gmail.com>
2026-09-10 15:10:46 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyB_JXt6qiP39tlVMcQDI40eej835kmMsXY",
-  authDomain: "://firebaseapp.com",
+  apiKey: "AIzaSyB_JXt6qiP39tlVMcQDI4OeEj835kmMsXY",
+  authDomain: "tart-manager.firebaseapp.com",
   projectId: "tart-manager",
   storageBucket: "tart-manager.firebasestorage.app",
   messagingSenderId: "968257225904",
   appId: "1:968257225904:web:2bad490ccbae3d5da6e775"
 };
 
-// アプリのプログラム（v6）が読み込めるように正しくエクスポートします
-export default firebaseConfig;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
