d776d3b262d32010fef336c8f1436c4b0ce6da76
tocchi9755 <tocchi9755@gmail.com>
2026-09-12 14:33:17 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.TART_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB_JXt6qiP39tlVMcQDI4OeEj835kmMsXY",
-  authDomain: "tart-manager.firebaseapp.com",
-  databaseURL: "https://tart-manager-default-rtdb.firebaseio.com",
-  projectId: "tart-manager",
-  storageBucket: "tart-manager.firebasestorage.app",
-  messagingSenderId: "968257225904",
-  appId: "1:968257225904:web:2bad490ccbae3d5da6e775"
+apiKey: "AIzaSyB_JXt6qiP39tlVMcQDI4OeEj835kmMsXY",
+authDomain: "tart-manager.firebaseapp.com",
+databaseURL: "https://tart-manager-default-rtdb.firebaseio.com",
+projectId: "tart-manager",
+storageBucket: "tart-manager.firebasestorage.app",
+messagingSenderId: "968257225904",
+appId: "1:968257225904:web:2bad490ccbae3d5da6e775"
 };
